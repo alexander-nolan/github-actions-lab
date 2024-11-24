dbce59a833590c3ff4340fb85d9ca95fae5dee34
Alexander Nolan <al.nolan@gmail.com>
2024-11-24 19:51:10 +0000

    Reconfigure backend bucket

--- main/backend.tf
+++ main/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket019178"
+    bucket = "my-terraform-state-bucket019179"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
