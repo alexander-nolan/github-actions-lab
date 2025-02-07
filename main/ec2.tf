f665d387f9f51afda95a52203688fa812b8db43a
Alexander Nolan <al.nolan@gmail.com>
2025-02-07 21:38:48 +0000

    Add ssh key and install minikube

--- main/ec2.tf
+++ main/ec2.tf
@@ -17,6 +17,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  key_name      = aws_key_pair.deployer.key_name
   user_data_replace_on_change = true
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web.id]
@@ -25,13 +26,22 @@ resource "aws_instance" "web" {
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
-              apt-get install -y nginx
-              echo "<h1>Welcome to the GitHub Actions & Terraform Lab!!</h1>" > /var/www/html/index.html
-              systemctl enable nginx
-              systemctl start nginx
+              apt-get install -y apt-transport-https ca-certificates curl
+              curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+              install minikube-linux-amd64 /usr/local/bin/minikube
+              minikube start --driver=none
               EOF
 
   tags = merge(var.project_tags, {
     Name = "web-server"
   })
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")  # Make sure this key exists locally
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
 }
